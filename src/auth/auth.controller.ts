69434c50b416a2c65d9aa86a22d6515353c9b41a
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2023-12-21 03:18:09 +0900

    feat: 리프레시 토큰 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/auth.dto';
+import { RefreshJwtGuard } from './guards/refresh.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,13 @@ export class AuthController {
   @Post('login')
   async registerUser(@Body() dto: LoginDto) {
     return await this.authService.login(dto);
+  }
+
+  @UseGuards(RefreshJwtGuard)
+  @Post('refresh')
+  async refreshToken(@Request() req) {
+    console.log('refreshed');
+
+    return await this.authService.refreshToken(req.user);
   }
 }
