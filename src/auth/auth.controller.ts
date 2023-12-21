2fc9926954072b639b55183518d8c12c4107c077
seokmin <msm0748@gmail.com>
2023-12-22 06:09:50 +0900

    refactor: jwt 로직 수정

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { LoginDto } from './dto/auth.dto';
 import { RefreshJwtGuard } from './guards/refresh.guard';
@@ -8,15 +9,23 @@ export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  async registerUser(@Body() dto: LoginDto) {
-    return await this.authService.login(dto);
+  async registerUser(@Body() dto: LoginDto, @Res({ passthrough: true }) response: Response) {
+    const { accessToken, refreshToken } = await this.authService.login(dto);
+    // response.cookie('accessToken', accessToken, { httpOnly: true });
+    // response.cookie('refreshToken', refreshToken, { httpOnly: true });
+
+    return {
+      message: 'login success',
+      accessToken,
+      refreshToken,
+    };
   }
 
-  @UseGuards(RefreshJwtGuard)
-  @Post('refresh')
-  async refreshToken(@Request() req) {
-    console.log('refreshed');
+  // @UseGuards(RefreshJwtGuard)
+  // @Post('refresh')
+  // async refreshToken(@Request() req) {
+  //   console.log('refreshed');
 
-    return await this.authService.refreshToken(req.user);
-  }
+  //   return await this.authService.refreshToken(req.user);
+  // }
 }
