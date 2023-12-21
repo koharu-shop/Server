ea52a66615cfb94a2b75b349d190d82e2fb3821d
seokmin <msm0748@gmail.com>
2023-12-22 03:02:36 +0900

    feat: cors 허용

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.NODE_ENV === 'development' ? true : [''],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
