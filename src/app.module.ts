c5b11fe4d4be69f3a893b5fb97deadabd96c9f32
seokmin <msm0748@gmail.com>
2023-12-24 16:59:15 +0900

    feat: 관리자, 상품 의존성 주입

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { AdminModule } from './admin/admin.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [UserModule, AuthModule, AdminModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
