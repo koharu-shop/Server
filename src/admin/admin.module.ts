47ac55fa12dff22df1af4a153637b6f37612cbd7
seokmin <msm0748@gmail.com>
2023-12-27 03:30:05 +0900

    feat: 전체 사용자 조회 API 추가

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -3,9 +3,11 @@ import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { PrismaService } from 'src/prisma.service';
 import { ProductService } from 'src/product/product.service';
+import { UserService } from 'src/user/user.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService, PrismaService, ProductService],
+  providers: [AdminService, PrismaService, ProductService, UserService, JwtService],
 })
 export class AdminModule {}
