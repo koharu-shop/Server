5ae0b9f3fafd34cba567ce8f76d302c7de243af0
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2024-01-09 11:10:46 +0900

    feat: 정적 파일 제공

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,21 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AdminModule } from './admin/admin.module';
 import { ProductModule } from './product/product.module';
+import { UploadsModule } from './uploads/uploads.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [UserModule, AuthModule, AdminModule, ProductModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    UserModule,
+    AuthModule,
+    AdminModule,
+    ProductModule,
+    UploadsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
