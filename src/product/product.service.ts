d648ddedfce4d95e2d51c2afcbbf26436209a191
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2024-01-31 22:49:25 +0900

    refactor: 관리자 API 분리

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,30 +1,9 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
-import { CreateCategoryDto } from './dto/product.dto';
 
 @Injectable()
 export class ProductService {
   constructor(private prismaService: PrismaService) {}
-
-  async addCategory(dto: CreateCategoryDto) {
-    try {
-      const newCategory = await this.prismaService.category.create({
-        data: {
-          ...dto,
-        },
-      });
-
-      return newCategory;
-    } catch (error) {
-      if (error.code === 'P2002') {
-        throw new NotFoundException('이미 해당 값이 존재하는 카테고리입니다.');
-      } else {
-        console.log(error, 'error');
-        // You may want to handle other types of errors differently
-        throw new Error('An unexpected error occurred.');
-      }
-    }
-  }
 
   async getCategory() {
     const category = await this.prismaService.category.findMany();
