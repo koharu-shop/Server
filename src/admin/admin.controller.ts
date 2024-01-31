340f18a9046f3b1fc474b311b2b2f80bd113bd90
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2024-01-31 23:09:03 +0900

    feat: 로그 설정

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, UseGuards, Request, UnauthorizedException } from '@nestjs/common';
+import { Body, Controller, Get, Post, UseGuards, Request, UnauthorizedException, Logger } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { CreateCategoryDto } from 'src/admin/dto/product.dto';
 import { UserService } from 'src/user/user.service';
@@ -6,6 +6,8 @@ import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @Controller('admin')
 export class AdminController {
+  private logger = new Logger('AdminController');
+
   constructor(
     private adminService: AdminService,
     private userService: UserService,
@@ -21,6 +23,7 @@ export class AdminController {
   @Post('category')
   create(@Body() createCategoryDto: CreateCategoryDto, @Request() req) {
     this.checkAdminRole(req);
+    this.logger.verbose(`카테고리 ${createCategoryDto.name} 항목 생성`);
     return this.adminService.addCategory(createCategoryDto);
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const PORT = 8000;
 
   app.enableCors({
     origin: process.env.NODE_ENV === 'development' ? true : [''],
@@ -20,6 +21,11 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(8000);
+  if (process.env.NODE_ENV === 'development') {
+    app.useLogger(new Logger());
+  }
+
+  await app.listen(PORT);
+  Logger.log(`Application running on port ${PORT}`);
 }
 bootstrap();
