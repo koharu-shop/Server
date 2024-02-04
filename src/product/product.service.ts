c4a4d2ada61207ce1872f433734c21a039904201
Moon Seokmin <78075709+msm0748@users.noreply.github.com>
2024-02-04 11:47:38 +0900

    feat: 상품 API 이미지 배열로 전달

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -10,7 +10,22 @@ export class ProductService {
       where: { id: id },
     });
 
-    return product;
+    const images = [product.img1, product.img2, product.img3, product.img4, product.img5].filter(img => img);
+
+    // 이미지들을 images 배열로 교체
+    const productWithImages = {
+      ...product,
+      images: images,
+    };
+
+    // 기존의 이미지 속성들은 제거
+    delete productWithImages.img1;
+    delete productWithImages.img2;
+    delete productWithImages.img3;
+    delete productWithImages.img4;
+    delete productWithImages.img5;
+
+    return productWithImages;
   }
 
   async getProductOptions(id: number) {
